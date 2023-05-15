bb121188b59ff1eec27855abe828f9efd325714a
zenghyun <pipo36@naver.com>
2023-05-15 23:41:10 +0900

    work REST API & Mongodb

---
+++ blog/blog-backend/src/api/index.js
@@ -0,0 +1,15 @@
+import Router from 'koa-router';
+import posts from './posts';
+import auth from './auth';
+
+const api = new Router();
+
+api.use('/posts', posts.routes());
+// api.get('/test', ctx => {
+//     ctx.body = 'test 성공';
+// });
+api.use('/auth', auth.routes());
+
+
+// 라우터를 내보냅니다. 
+export default api;
