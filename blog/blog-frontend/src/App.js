a79aeda5b7546aed31eb25bdfd3f1bde760a374e
zenghyun <pipo36@naver.com>
2023-05-16 12:09:54 +0900

    Update Frontend

---
+++ blog/blog-frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+import PostPage from "./pages/PostPage";
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route element={<PostListPage />} path={['/@:username', '/']} exact />
+      <Route element={<LoginPage />} path="/login" />
+      <Route element={<RegisterPage />} path="/register" />
+      <Route element={<WritePage />} path="/write" />
+      <Route element={<PostPage />} path="/@:username/:postId" />
+   </Routes>
+    </>
+   
+  );
+}
+
+export default App;
